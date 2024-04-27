6cf50986b44b9597c90657e80e0099243309e83b
Ruddy-M-L <ruddyrealy@gmail.com>
2024-04-27 18:24:48 -0400

    grupo_licenciado

--- grupo_licenciado/inicio.php
+++ grupo_licenciado/inicio.php
@@ -1,7 +1,7 @@
 <?php
 
 print("Somos grupo licen conformado por Ruddy Mauricio Laura Quispe, Jorge Alberto Coronado Guzman, Abasto Vargas Julia Emalier,
-Dennis Joel Cruz Flores");
+Jose Emanuel Mamani Alcon");
 
 
 ?>
