112892ea3ff42dc3687779b669d1ac934c2b9613
caalvarezudabol <166525768+caalvarezudabol@users.noreply.github.com>
2024-05-02 13:49:08 -0400

    Update inicio.php

--- Grupo_CarlosAmaya_CritianRamos/inicio.php
+++ Grupo_CarlosAmaya_CritianRamos/inicio.php
@@ -8,8 +8,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
 </head>
 <body>
-    
+    <h1>Genial.......</h1>
 </body>
 </html>
