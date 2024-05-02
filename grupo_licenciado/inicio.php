4149ffaf652cd5e6ebc1a0f1d31db0c25efe051d
caalvarezudabol <166525768+caalvarezudabol@users.noreply.github.com>
2024-05-02 13:50:24 -0400

    Update inicio.php

--- grupo_licenciado/inicio.php
+++ grupo_licenciado/inicio.php
@@ -2,6 +2,6 @@
 
 print("Somos grupo licen conformado por Ruddy Mauricio Laura Quispe, Jorge Alberto Coronado Guzman, Abasto Vargas Julia Emalier,
 Jose Emanuel Mamani Alcon");
-
+print("Genial!!! +20");
 
 ?>
