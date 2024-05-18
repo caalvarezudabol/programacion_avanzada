92074230a18682405ddaa0963c56cfce05298005
Ruddy-M-L <ruddyrealy@gmail.com>
2024-05-18 13:23:56 -0400

    Ruddy-M-L

--- database/migrations/2024_05_18_130515_create_prueba_r_s_table.php
+++ database/migrations/2024_05_18_130515_create_prueba_r_s_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('nombre');
             $table->text('descripcion');
-            $table->tinIngeteger('edad');
+            $table->tinInteger('edad');
             $table->timestamps();
         });
     }
