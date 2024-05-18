3a7b37119890a3ee4b090272315210dc930028a6
Javieerr12345 <jchoque43-es@udabol.edu.bo>
2024-05-18 13:48:38 -0400

    se modifico con los integrantes de JulioCesar Choque, Javier Choque, Luis Gabriel Flores

---
+++ database/migrations/2024_05_18_132941_create_pruebas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pruebas', function (Blueprint $table) {
+            $table->id();
+            $table->nombre('javier Choque Flores');
+            $table->nombre('Julio Cesar Choque Grimaldo');
+            $table->nombre('Luis Gabriel Flores Valdes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pruebas');
+    }
+};
